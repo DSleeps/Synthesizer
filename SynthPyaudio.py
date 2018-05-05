7d29b07fc7bd97ea402a1f22a5c592156b7fce03
DSleeps <Dill_Pill_007@yahoo.com>
2018-05-05 14:29:41 -0400

    Sorta Working

--- SynthPyaudio.py
+++ SynthPyaudio.py
@@ -4,11 +4,16 @@ import pyaudio
 import fluidsynth
 import sys
 
+sample_size = 44100
+#sample_size = 22000
+
+velocity = 100
+
 pa = pyaudio.PyAudio()
 strm = pa.open(
     format = pyaudio.paInt32,
     channels = 2,
-    rate = 44100,
+    rate = sample_size,
     output = True)
 
 s = []
@@ -16,24 +21,24 @@ s = []
 fl = fluidsynth.Synth()
 
 # Initial silence is 1 second
-s = numpy.append(s, fl.get_samples(44100 * 1))
+s = numpy.append(s, fl.get_samples(sample_size * 1))
 
 sfid = fl.sfload(sys.argv[1])
 fl.program_select(0, sfid, 0, 0)
 
-fl.noteon(0, 60, 30)
-fl.noteon(0, 67, 30)
-fl.noteon(0, 76, 30)
+fl.noteon(0, 60, velocity)
+fl.noteon(0, 67, velocity)
+fl.noteon(0, 76, velocity)
 
 # Chord is held for 2 seconds
-s = numpy.append(s, fl.get_samples(44100 * 2))
+s = numpy.append(s, fl.get_samples(sample_size * 2))
 
 fl.noteoff(0, 60)
 fl.noteoff(0, 67)
 fl.noteoff(0, 76)
 
 # Decay of chord is held for 1 second
-s = numpy.append(s, fl.get_samples(44100 * 1))
+s = numpy.append(s, fl.get_samples(sample_size * 1))
 
 fl.delete()
 
@@ -42,3 +47,4 @@ samps = fluidsynth.raw_audio_string(s)
 print(len(samps))
 print('Starting playback')
 strm.write(samps)
+
