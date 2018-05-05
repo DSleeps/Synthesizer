18f5d97516bc5f244ad9d5ea84d252e0afebfca2
dsleeper <35410738+DSleeps@users.noreply.github.com>
2018-05-05 14:34:37 -0400

    Test

---
+++ SynthPlaying.py
@@ -0,0 +1,54 @@
+import time
+import numpy
+import pyaudio
+import fluidsynth
+import sys
+
+sample_size = 44100
+#sample_size = 22000
+
+velocity = 100
+
+pa = pyaudio.PyAudio()
+strm = pa.open(
+    format = pyaudio.paInt32,
+    channels = 2,
+    rate = sample_size,
+    output = True)
+
+s = []
+
+fl = fluidsynth.Synth()
+
+# Initial silence is 1 second
+s = numpy.append(s, fl.get_samples(sample_size * 1))
+
+sfid = fl.sfload(sys.argv[1])
+fl.program_select(0, sfid, 0, 0)
+
+fl.noteon(0, 60, velocity)
+fl.noteon(0, 67, velocity)
+fl.noteon(0, 76, velocity)
+
+# Chord is held for 2 seconds
+print('Starting playback 1')
+for i in range(1000):
+    s = fl.get_samples(sample_size * .01)
+    samps = fluidsynth.raw_audio_string(s)
+    strm.write(samps)
+
+
+fl.noteoff(0, 60)
+fl.noteoff(0, 67)
+fl.noteoff(0, 76)
+
+# Decay of chord is held for 1 second
+s = numpy.append(s, fl.get_samples(sample_size * 1))
+
+fl.delete()
+
+samps = fluidsynth.raw_audio_string(s)
+
+print(len(samps))
+print('Starting playback')
+strm.write(samps)
