0e386b1c5d821c01e059e7d4f9964c5617dfe6cf
Dylan Sleeper <Dill_Pill_007@yahoo.com>
2018-05-29 23:31:07 -0700

    Synth Working

--- Looping.py
+++ Looping.py
@@ -10,19 +10,44 @@ sample_size = 44100
 #sample_size = 22000
 
 #Times per second that it checks the midi thing
-times_per_sec = 100
+times_per_sec = 50
+
+#The speed of the song
+bpm = 120.0
+
+#The amount of measures in the loop
+measures = 4.0
+
+#The time signature
+beats_per_measure = 4.0
+
+#Subdivide amount
+subdivide = 4
+
+current_run_count = 0
+
+#The delay of the main loop to ensure it doesn't overwrite the other stuff
+loop_delay = (60.0/(bpm*subdivide))
+loop_delay_timer = time.time()
+
+#The amount of time the previous seconds equate to in recording in seconds
+loop_time = (60.0/bpm) * measures * beats_per_measure
+loop_timer = time.time()
+
+stream_count = 8
 
 #Opens the audio stream to write to
 pa = pyaudio.PyAudio()
-strm = pa.open(
-    format = pyaudio.paInt32,
-    channels = 2,
-    rate = sample_size,
-    output = True)
 
 #This enables multiple streams at once
 streams = []
-streams.append(strm)
+for i in range(stream_count):
+    strm = pa.open(
+        format = pyaudio.paInt32,
+        channels = 2,
+        rate = sample_size,
+        output = True)
+    streams.append(strm)
 
 midiin = rtmidi.RtMidiIn()
 
@@ -43,6 +68,18 @@ def print_message(midi):
 class AudioPlayer (threading.Thread):
 
     last_time = time.time()
+    audio_state = 0
+
+    track = []
+    track_str = ''
+    track_pos = 0
+
+    is_done = False
+
+    #State 0: Playing
+    #State 1: Recording
+    #State 2: Playback
+    #State 3: Idle
 
     def __init__(self, track_id, synth, instrument):
         threading.Thread.__init__(self)
@@ -50,16 +87,71 @@ class AudioPlayer (threading.Thread):
         self.synth = synth
         self.instrument = instrument
 
+#Originally had this in all of the states but it seemed to glitch the audio out
+
+#Waits to make sure it isn't too fast
+# if time.time() - self.last_time < 1.0/times_per_sec:
+#     #Sleep for the remaining time
+#     time.sleep(1.0/times_per_sec - (time.time() - self.last_time))
+#     self.last_time = time.time()
+
+
     def run(self):
         while True:
-            s = []
-            s = numpy.append(s, self.synth.get_samples(int(sample_size/times_per_sec)))
-            samps = fluidsynth.raw_audio_string(s)
-            streams[self.track_id].write(samps)
-            while time.time() - self.last_time < 1.0/times_per_sec:
-                pass
+            #Stop the thread
+            if self.is_done == True:
+                break
+
+            #The playing state
+            if self.audio_state == 0:
+                s = []
+                s = numpy.append(s, self.synth.get_samples(int(sample_size/times_per_sec)))
+                samps = fluidsynth.raw_audio_string(s)
+                streams[self.track_id].write(samps)
+
+            #The recording state
+            elif self.audio_state == 1:
+                s = []
+                s = numpy.append(s, self.synth.get_samples(int(sample_size/times_per_sec)))
+                samps = fluidsynth.raw_audio_string(s)
+                streams[self.track_id].write(samps)
+
+                #This adds everything that shows up in s into this track
+                self.track = numpy.append(self.track, s)
+
+            #The start playback state
+            elif self.audio_state == 2:
+                track_pos = 0
+                self.audio_state = 3
+
+            #The playback state
+            elif self.audio_state == 3:
+                if track_pos < len(self.track):
+                    track_segment = self.track[track_pos:track_pos + int(len(self.track)/times_per_sec)]
+
+                    #Updates where it is in the track
+                    track_pos += int(len(self.track)/times_per_sec)
+
+                    #Converts it into a string that can be outputted
+                    self.track_str = fluidsynth.raw_audio_string(track_segment)
+                    streams[self.track_id].write(self.track_str)
+
+    def start_recording(self):
+        self.audio_state = 1
+
+    #Puts you into the playback state
+    def stop_recording(self):
+        self.audio_state = 2
+
+    def start_playback(self):
+        self.audio_state = 2
+
+    def stop_thread(self):
+        self.is_done = True
 
 class MidiInput (threading.Thread):
+
+    is_done = False
 
     def __init__(self, midi_controller, synth):
         threading.Thread.__init__(self)
@@ -68,15 +160,22 @@ class MidiInput (threading.Thread):
 
     def run(self):
         while True:
+            #Stops the thread
+            if self.is_done == True:
+                break
+
             m = self.midi_controller.getMessage(1) # some timeout in ms
             if m:
-                print_message(m)
+                #print_message(m)
                 if m.isNoteOn():
                     is_pressed = True
                     self.synth.noteon(0, m.getNoteNumber(), m.getVelocity())
                 elif m.isNoteOff():
                     is_pressed = False
                     self.synth.noteoff(0, m.getNoteNumber())
+
+    def stop_thread(self):
+        self.is_done = True
 
 
 ports = range(midiin.getPortCount())
@@ -88,11 +187,56 @@ if ports:
     print("Opening port 0!")
     midiin.openPort(1)
 
+    audio_players = []
+
     audio_player = AudioPlayer(0, fl, 'Guitar')
     midi_input = MidiInput(midiin, fl)
 
     audio_player.start()
     midi_input.start()
 
-    audio_player.join()
-    midi_input.join()
+    audio_players.append(audio_player)
+
+    print("Started recording...")
+    audio_player.start_recording()
+
+    loop_times = 0
+
+    while True:
+        #print("Loop ran")
+        if current_run_count % subdivide == 0:
+            print(int(current_run_count/4))
+
+        if time.time() - loop_timer > loop_time:
+            print("Stopped Recording")
+
+            #Restart all of the players
+            for player in audio_players:
+                player.stop_recording()
+                player.start_playback()
+
+            #Create a new player for the next track
+            new_player = AudioPlayer(len(audio_players), fl, 'Guitar')
+            new_player.start_recording()
+            new_player.start()
+
+            audio_players.append(new_player)
+
+            loop_timer = time.time()
+            loop_times += 1
+            current_run_count = 0
+
+        #Waits to make sure it isn't too fast
+        if time.time() - loop_delay_timer < loop_delay:
+            #Sleep for the remaining time
+            time.sleep(loop_delay - (time.time() - loop_delay_timer))
+            loop_delay_timer = time.time()
+
+        if loop_times > 6:
+            break
+
+        current_run_count += 1
+
+    for player in audio_players:
+        player.stop_thread()
+    midi_input.stop_thread()
--- Midi_Controller/Midi_Controller.ino
+++ Midi_Controller/Midi_Controller.ino
@@ -3,39 +3,62 @@
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 //Previous values of the buttons
-int p_1 = 0;
-int p_2 = 0;
+int p_1 = 1;
+int p_2 = 1;
 
 void setup() {
+    MIDI.begin(1);
     Serial.begin(115200);
     pinMode(2,INPUT_PULLUP);
     pinMode(3,INPUT_PULLUP);
-
-    MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
 void loop() {
     int button_val_1 = digitalRead(2);
     int button_val_2 = digitalRead(3);
-    Serial.println(button_val_1);
-    Serial.println(button_val_2);
 
-    if (button_val_1 == 0) {
-        MIDI.sendNoteOn(48, 100, 0);
+    byte a = (byte)button_val_1;
+
+    MIDI.read();
+    
+    if (button_val_1 == 0 && p_1 == 1) {
+        noteOn(48, 100, 1);
     }
-    if (button_val_2 == 0) {
-        MIDI.sendNoteOn(56, 100, 0);
+    if (button_val_2 == 0 && p_2 == 1) {
+        noteOn(56, 100, 1);
     }
 
     if (p_1 == 0 && button_val_1 == 1) {
-        MIDI.sendNoteOff(48, 0);
+        noteOff(48, 0, 1);
     }
     if (p_2 == 0 && button_val_2 == 1) {
-        MIDI.sendNoteOff(56, 0);
+        noteOff(56, 0, 1);
     }
-
-    MIDI.read();
 
     p_1 = button_val_1;
     p_2 = button_val_2;
 }
+
+void noteOn(int note, int velocity, int channel) {
+    MIDI.sendNoteOn(note, velocity, channel);
+}
+
+void noteOff(int note, int velocity, int channel) {
+    MIDI.sendNoteOff(note, velocity, channel);
+}
+
+//These are the functions for the MIDIUSB library which doesn't need the separate app
+
+//void noteOn(int note, int velocity, int channel) {
+//  midiEventPacket_t noteOn = {0x09, 0x90 | (byte)channel, (byte)note, (byte)velocity};
+//  MidiUSB.sendMIDI(noteOn);
+//}
+//
+//void noteOff(int note, byte velocity, int channel) {
+//  midiEventPacket_t noteOff = {0x08, 0x80 | (byte)channel, (byte)note, (byte)velocity};
+//  MidiUSB.sendMIDI(noteOff);
+//}
+//void controlChange(int conrol, int value, int channel) {
+//  midiEventPacket_t event = {0x0B, 0xB0 | (byte)channel, (byte)control, (byte)value};
+//  MidiUSB.sendMIDI(event);
+//}
